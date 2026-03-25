794c811d89a839260baa582bbb991245a8f27ef3
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-03-25 15:20:36 +0100

    add public ssh key

--- common/core/users.nix
+++ common/core/users.nix
@@ -3,6 +3,9 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "audio" "input" "lp" "scanner" "docker" ];
     shell = pkgs.fish; 
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNrpS0BG9BshjujiJ3KP/g04LIJFT9ZQ0cSYCuM4HKi sem@toasterBTW"
+    ];
   };
 
   programs.fish.enable = true;
