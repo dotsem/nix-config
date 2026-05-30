d7a9f8c0c1a89b3026f4cb21ab98e63d3598dd39
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-05-30 13:38:45 +0200

    add nginx-blocking to each server using nginx

---
+++ common/server/nginx-blocking.nix
@@ -0,0 +1,16 @@
+{
+  # block private files and directories
+  "~ /\\.(?!well-known(/|$))" = {
+    return = "404";
+  };
+
+  # block sensitive extensions and compressed backups
+  "~* \\.(env|key|pem|pypirc|bak|config|sql|yaml|yml|db|sqlite|log|sh|swp)(\\.(gz|zip|tar|tgz|7z|rar|bz2))?(;|/|$)" = {
+    return = "404";
+  };
+
+  # block unextended private ssh keys
+  "~* /(id_rsa|id_dsa|id_ecdsa|id_ed25519)(;|/|$)" = {
+    return = "404";
+  };
+}
--- machines/server/battlebus/retail-row/nginx.nix
+++ machines/server/battlebus/retail-row/nginx.nix
@@ -1,26 +1,23 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  securityLocations = import ../../../../common/server/nginx-hardening.nix;
+in {
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     virtualHosts."gostrategy.dotsem.be" = {
-      locations."~ /\\.(?!well-known(/|$))" = {
-        return = "404";
-      };
-
-      locations."~* \\.(env|key|pem|pypirc|bak|config|sql|yaml|yml)(;|/|$)" = {
-        return = "404";
-      };
-
-      locations."/" = {
-        proxyPass = "http://localhost:1000"; # Frontend
-      };
-      locations."/api/" = {
-        proxyPass = "http://localhost:1001/"; # Backend
-      };
-      locations."/ws/" = {
-        proxyPass = "http://localhost:1001/";
-        proxyWebsockets = true;
+      locations = securityLocations // {
+        "/" = {
+          proxyPass = "http://localhost:1000"; # Frontend
+        };
+        "/api/" = {
+          proxyPass = "http://localhost:1001/"; # Backend
+        };
+        "/ws/" = {
+          proxyPass = "http://localhost:1001/";
+          proxyWebsockets = true;
+        };
       };
     };
   };
