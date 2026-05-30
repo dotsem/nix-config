f7a21469e001bdba13b7cb82b0f54c3d9a04cc5f
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-05-30 17:26:35 +0200

    fix nginx regex

--- common/server/nginx-blocking.nix
+++ common/server/nginx-blocking.nix
@@ -5,12 +5,12 @@
   };
 
   # block sensitive extensions and compressed backups
-  "~* \\.(env|key|pem|pypirc|bak|config|sql|yaml|yml|db|sqlite|log|sh|swp)(\\.(gz|zip|tar|tgz|7z|rar|bz2))?(;|/|$)" = {
+  "~* \\.(env|key|pem|pypirc|bak|config|sql|yaml|yml|db|sqlite|log|sh|swp)(\\.(gz|zip|tar|tgz|7z|rar|bz2))?(/|$)" = {
     return = "404";
   };
 
   # block unextended private ssh keys
-  "~* /(id_rsa|id_dsa|id_ecdsa|id_ed25519)(;|/|$)" = {
+  "~* /(id_rsa|id_dsa|id_ecdsa|id_ed25519)(/|$)" = {
     return = "404";
   };
 }
