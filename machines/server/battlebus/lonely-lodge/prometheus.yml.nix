a8791e152c27307b3e8f03f262e079a20e02f5d4
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-08-06 16:01:16 +0200

    fix syntax error

--- machines/server/battlebus/lonely-lodge/prometheus.yml.nix
+++ machines/server/battlebus/lonely-lodge/prometheus.yml.nix
@@ -1,12 +1,12 @@
 { pkgs, hosts }:
-  pkgs.writeText "prometheus.yml" ''
-    global:
-      scrape_interval: 15s
-      evaluation_interval: 15s
+pkgs.writeText "prometheus.yml" ''
+  global:
+    scrape_interval: 15s
+    evaluation_interval: 15s
 
-    scrape_configs:
-      - job_name: "gostrategy-backend"
-        static_configs:
-          - targets: ["${hosts.retail-row.ip}:80"]
-        metrics_path: /metrics
-  '';
+  scrape_configs:
+    - job_name: "gostrategy-backend"
+      static_configs:
+        - targets: ["${hosts.retail-row.ip}:80"]
+      metrics_path: /metrics
+''
