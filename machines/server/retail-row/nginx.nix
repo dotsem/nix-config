56bf79556376559ac938b49c9b50875715c87602
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-08-07 21:12:22 +0200

    feat: add prometheus logging

--- machines/server/retail-row/nginx.nix
+++ machines/server/retail-row/nginx.nix
@@ -1,6 +1,6 @@
 { config, pkgs, hosts, ... }:
 let
-  securityLocations = import ../../../../common/server/nginx-blocking.nix;
+  securityLocations = import ../../../common/server/nginx-blocking.nix;
 in {
   services.nginx = {
     enable = true;
