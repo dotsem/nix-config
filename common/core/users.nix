197ed12c489930b3ac8ec83773fd7745d61e3815
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-08-07 20:11:24 +0200

    add public key to root account

--- common/core/users.nix
+++ common/core/users.nix
@@ -1,13 +1,19 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  keys = import ../../lib/keys.nix;
+in
+{
   users.users.sem = {
     isNormalUser = true;
     # docker is declared in desktop/virtualization.nix; servers declare it locally
     extraGroups = [ "wheel" "networkmanager" "video" "audio" "input" "lp" "scanner" ];
     shell = pkgs.fish;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNrpS0BG9BshjujiJ3KP/g04LIJFT9ZQ0cSYCuM4HKi sem@toasterBTW"
-    ];
+    openssh.authorizedKeys.keys = [ keys.toasterBTW ];
     hashedPassword = "$6$3U2G7TDA2NQY.Z9Y$ZvMhe.iXhuvCFrukJKilu.VUBGSfW4u8LVaqDuBrSG3eb2D2ama2/99uiIvQJPQyrhE5mI.RYyv6WLy9TKQ411";
+  };
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [ keys.toasterBTW ];
   };
 
   programs.fish.enable = true;
---
+++ lib/keys.nix
@@ -0,0 +1,4 @@
+# Centralized SSH public keys repository.
+{
+  toasterBTW = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNrpS0BG9BshjujiJ3KP/g04LIJFT9ZQ0cSYCuM4HKi sem@toasterBTW";
+}
