04c03e092cfca44f4cc30b7414a22bd5e7c1d61d
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-09-12 21:59:02 +0200

    refactor(grocy): disable SSL, update culture to nl, and disable unused feature flags

--- machines/server/greasy-grove/grocy.nix
+++ machines/server/greasy-grove/grocy.nix
@@ -9,18 +9,26 @@
   services.grocy = {
     enable = true;
     hostName = "greasy-grove.home";
+    nginx.enableSSL = false;
     settings = {
       currency = "EUR";
-      culture = "en";
+      culture = "nl";
       calendar = {
         firstDayOfWeek = 1; # monday
         showWeekNumber = true;
       };
       entryPage = "stock";
     };
+    extraConfig = ''
+      Setting("FEATURE_FLAG_CHORES", false);
+      Setting("FEATURE_FLAG_TASKS", false);
+      Setting("FEATURE_FLAG_BATTERIES", false);
+      Setting("FEATURE_FLAG_EQUIPMENT", false);
+      Setting("FEATURE_FLAG_CALENDAR", false);
+      Setting("FEATURE_FLAG_STOCK_PRICE_TRACKING", false);
+    '';
   };
 
-  # allow direct ip access and alternative hostnames
   services.nginx.virtualHosts."${config.services.grocy.hostName}" = {
     default = true;
     serverAliases = [
