acf94cdf9f0fe5e461656d1ddcf8b1b1230e6692
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-09-17 19:39:57 +0200

    fix no unhashed passwords

--- common/server/sops.nix
+++ common/server/sops.nix
@@ -3,15 +3,17 @@
   options.custom.server.sopsPassword = {
     enable = lib.mkOption {
       type = lib.types.bool;
-      default = false;
+      default = true;
       description = "enable sops-managed password for user sem";
     };
   };
 
   config = lib.mkIf config.custom.server.sopsPassword.enable {
+    users.mutableUsers = false;
+
     sops.secrets.sem_password = {
       neededForUsers = true;
     };
-    users.users.sem.passwordFile = config.sops.secrets.sem_password.path;
+    users.users.sem.hashedPasswordFile = config.sops.secrets.sem_password.path;
   };
 }
